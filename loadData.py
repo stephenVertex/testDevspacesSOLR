a9cf62b0e84a8516cccb17bd307884f9785d4a90
Stephen Barr (Vertex Development) <stephen.barr@devfactory.com>
2022-07-19 19:40:31 +0000

    Fixing up names for consistency

--- loadData.py
+++ loadData.py
@@ -2,5 +2,26 @@
 
 import json
 import pysolr
+import requests
+# solr = pysolr.Solr("https://localhost:8983/solr/")
 
-solr = pysolr.Solr("https://8983-chocolate-mammal-z2ne1gf6.ws.ey-graviton.devspaces.com/solr/")
+with open('wikipedia-movie-data/movies.json', 'r') as f:
+    jd = f.read()
+    x = json.loads(jd)
+
+solr = pysolr.Solr('http://localhost:8983/solr/my_core/', timeout=10)
+print(solr.ping())
+
+
+solr.add(x)
+
+solr.commit()
+
+
+print("Searching for Buffalo movies")
+r = requests.get("http://localhost:8983/solr/my_core/query?q=title:*buffalo*&q.op=OR&indent=true&rows=20")
+res = r.json()
+
+
+for rx in res['response']['docs']:
+    print(f"{rx['title']} - {rx['year']}")
